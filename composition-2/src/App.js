7a355225d5f0e45c0c552e28ce13bb553f199cd1
marcoglp98 <marcoglp98@gmail.com>
2023-03-11 11:15:46 +0100

    add composition exercises

---
+++ composition-2/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from './Container';
+import Login from './Login';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Container title='placeholder'>
+                <h1>Ciao!</h1>
+            </Container>
+        )
+    }
+}
+
+export default App;
---
+++ composition/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from './Container';
+
+
+export class App extends React.Component {
+    render() {
+        return (
+            <Container>
+               <h1>Ciao</h1> 
+            </Container>
+        )
+    }
+}
