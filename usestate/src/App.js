be34aedb1b46d07debf43bfa355ee7732d70fb1a
marcoglp98 <marcoglp98@gmail.com>
2023-03-11 16:03:37 +0100

    add usestate exercises

---
+++ usestate/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ClickCounter from './ClickCounter';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ClickCounter />
+      </div>
+    )
+  }
+}
+
---
+++ usestate/src/ClickCounter.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export const ClickCounter = () => {
+  const [counter, setCounter] = useState(0)
+
+  const handleClick = () => {
+    setCounter(cnt => cnt + 1)
+  }
+
+    return (
+      <div>
+        <h1>{counter}</h1>
+        <button onClick={handleClick}>Cliccami</button>
+      </div>
+    );
+  }
+
