584fc40a050171357db2bf2cb7bcfa381b77cca3
marcoglp98 <marcoglp98@gmail.com>
2023-03-12 17:45:51 +0100

    add usecallback exercise

---
+++ usecallback/src/useCounter.js
@@ -0,0 +1,33 @@
+import { useState, useCallback } from "react";
+
+export const useCounter = (initialValue = 0, increment = 1, decrement = 2) => {
+  const [counter, setCounter] = useState(initialValue);
+
+  const handleCounterIncrement = useCallback(
+    function handleCounterIncrement() {
+      setCounter((cnt) => cnt + increment);
+    },
+    [increment]
+  );
+
+  const handleCounterDecrement = useCallback(
+    function handleCounterDecrement() {
+      setCounter((cnt) => cnt - decrement);
+    },
+    [decrement]
+  );
+
+  const handleCounterReset = useCallback(
+    function handleCounterReset() {
+      setCounter(initialValue);
+    },
+    [initialValue]
+  );
+
+  return {
+    counter: counter,
+    onIncrement: handleCounterIncrement,
+    onDecrement: handleCounterDecrement,
+    onReset: handleCounterReset,
+  };
+};
